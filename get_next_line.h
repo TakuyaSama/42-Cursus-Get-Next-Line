7b8a910672f6638d3849493e6d3a7bf0998cf03e
Alberto Diaz lopez <adiaz-lo@c3r14s4.42madrid.com>
2020-01-21 13:44:44 +0100

    First commit with code and adding the base files, along with a test file.

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: adiaz-lo <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/01/21 12:52:28 by adiaz-lo          #+#    #+#             */
+/*   Updated: 2020/01/21 13:28:43 by adiaz-lo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+int	get_next_line(int fd, char **line);
+
+
+#endif
